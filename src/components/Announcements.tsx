1025186ab1c514940e938df1fee9c7a24f45a504
KarlStevenRequinto <lerdrobott@gmail.com>
2025-04-08 21:35:16 +0800

    styling edit

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -6,7 +6,7 @@ const Announcements = () => {
                 <span className="text-xs text-gray-400">View All</span>
             </div>
             <div className="flex flex-col gap-4 mt-4">
-                <div className="bg-lamaSkyLight rounded-md p-4">
+                <div className="bg-skyLight rounded-md p-4">
                     <div className="flex items-center justify-between">
                         <h2 className="font-medium">Lorem ipsum dolor sit</h2>
                         <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
@@ -15,7 +15,7 @@ const Announcements = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, expedita. Rerum, quidem facilis?
                     </p>
                 </div>
-                <div className="bg-lamaPurpleLight rounded-md p-4">
+                <div className="bg-purpleLight rounded-md p-4">
                     <div className="flex items-center justify-between">
                         <h2 className="font-medium">Lorem ipsum dolor sit</h2>
                         <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
@@ -24,7 +24,7 @@ const Announcements = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, expedita. Rerum, quidem facilis?
                     </p>
                 </div>
-                <div className="bg-lamaYellowLight rounded-md p-4">
+                <div className="bg-yellowLight rounded-md p-4">
                     <div className="flex items-center justify-between">
                         <h2 className="font-medium">Lorem ipsum dolor sit</h2>
                         <span className="text-xs text-gray-400 bg-white rounded-md px-1 py-1">2025-01-01</span>
